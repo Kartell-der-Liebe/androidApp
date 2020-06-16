758a22b712e471fc4de6472f1e3d2562a7febc09
Bjoern Petersen <bjoern.petersen@missinglinkelectronics.com>
2020-06-16 18:19:53 +0200

    Erstes BottonMenu hinzugefügt im Line up, aber noch ohne Funktion

--- src/app/src/main/java/com/example/kartellderliebe/timeTableFragment.kt
+++ src/app/src/main/java/com/example/kartellderliebe/timeTableFragment.kt
@@ -6,13 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class FestivalInfosFragment : Fragment() {
+class timeTableFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_festival_infos, container, false)
+        return inflater.inflate(R.layout.fragment_time_table, container, false)
     }
 }
