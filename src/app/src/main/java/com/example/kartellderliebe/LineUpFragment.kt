7f3252913a8b29e361eec4036a61f7beebaad50d
Bjoern Petersen <bjoern.petersen@missinglinkelectronics.com>
2020-06-17 11:39:01 +0200

    fehlende { eingefügt

--- src/app/src/main/java/com/example/kartellderliebe/LineUpFragment.kt
+++ src/app/src/main/java/com/example/kartellderliebe/LineUpFragment.kt
@@ -13,18 +13,7 @@ class LineUpFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        println("Ho")
-        val view = inflater.inflate(R.layout.fragment_line_up, container, false)
         // Inflate the layout for this fragment
-        val navListener = BottomNavigationView.OnNavigationItemReselectedListener { menuItem ->
-            when (menuItem.itemId){
-                R.id.lineUpFragment1 ->{
-                    println("Hallo 1")
-                }
-                R.id.lineUpFragment2 ->{
-                    println("Hallo 2")
-                }
-            }
-        }
-        return view
+        return inflater.inflate(R.layout.fragment_line_up, container, false)
+    }
 }
