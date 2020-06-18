ccc63af082fd2168caa882a2bb819620436a7688
Bjoern Petersen <bjoern.petersen@missinglinkelectronics.com>
2020-06-19 01:00:15 +0200

    Menu funktioniert immernoch nicht

--- src/app/src/main/java/com/example/kartellderliebe/SettingsFragment.kt
+++ src/app/src/main/java/com/example/kartellderliebe/SettingsFragment.kt
@@ -2,12 +2,19 @@ package com.example.kartellderliebe
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceFragmentCompat
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : Fragment() {
 
-    @SuppressLint("ResourceType")
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.layout.fragment_settings, rootKey)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 }
