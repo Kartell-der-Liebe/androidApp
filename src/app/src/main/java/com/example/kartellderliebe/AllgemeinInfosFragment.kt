1b8b94480e10cdf60dd360f64fb6c471b1523274
Bjoern Petersen <bjoern.petersen@missinglinkelectronics.com>
2020-06-22 04:40:12 +0200

    Webinhalt wird nicht in Textview geladen

--- src/app/src/main/java/com/example/kartellderliebe/AllgemeinInfosFragment.kt
+++ src/app/src/main/java/com/example/kartellderliebe/AllgemeinInfosFragment.kt
@@ -1,17 +1,15 @@
 package com.example.kartellderliebe
 
 import android.annotation.SuppressLint
-import android.annotation.TargetApi
-import android.os.Build
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebResourceError
-import android.webkit.WebResourceRequest
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Toast
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.kartellderliebe.databinding.FragmentAllgemeinInfosBinding
 
@@ -23,16 +21,37 @@ class AllgemeinInfosFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val html = "<iframe src=\"https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FBerlin&amp;src=YmpvZXJuMi5wZXRlcnNlbkBnbWFpbC5jb20&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=ZGUuZ2VybWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=bjlwN3NuMGtmNGhtN2dmYmVubzZxazJ2MHRlMG00YnRAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&amp;src=bWk5amowczdmY2prOTNnZnU4dGN1MXMyaGxmYnMzbGVAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&amp;src=bW82bjBucmI1ZXU5aW0wZDI2YmpibnQ4a2Z2YWU1YnNAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&amp;src=Y3NuMzI0OG9tM3RkMGM3b2locWVkajM5ZTA4YjE1MmFAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&amp;src=YWRrOWhrczBkbGY1dmtzcGIzc3Npbmxpdmc0c2FidW5AaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&amp;color=%23EF6C00&amp;color=%233F51B5&amp;color=%23009688&amp;color=%23C0CA33&amp;color=%23AD1457&amp;color=%23D81B60&amp;color=%239E69AF&amp;color=%23AD1457\" style=\"border:solid 1px #777\" width=\"400\" height=\"500\" frameborder=\"0\" scrolling=\"no\"></iframe>"
+        val html = "<iframe src=\"https://google.com&output=embed\" style=\"border:solid 1px #777\" width=\"400\" height=\"500\" frameborder=\"0\" scrolling=\"no\"></iframe>"
         // Inflate the layout for this fragment
         val binding = FragmentAllgemeinInfosBinding.inflate(inflater, container, false)
-        val webView = binding.WebviewInfosAllgemein
-        /*webView.webChromeClient = WebChromeClient()
-        webView.webViewClient = WebViewClient()
-        webView.settings.javaScriptEnabled = true
-        binding.WebviewInfosAllgemein.loadData(html, "text/html", null)*/
+        val webView = binding.webView
+        val contentView = binding.contentView
+        contentView.movementMethod = ScrollingMovementMethod()
 
+        class MyJavaScriptInterface(aContentView: TextView) {
+            private var contentView1: TextView? = null
+
+            init {
+                contentView1 = aContentView
+            }
+
+            @JavascriptInterface
+            fun processContent(aContent: String) {
+                contentView1?.post { contentView.text = aContent }
+            }
+        }
+
+        webView.settings.javaScriptEnabled = true
+        webView.addJavascriptInterface(MyJavaScriptInterface(contentView), "INTERFACE")
         webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                view.loadUrl("javascript:window.INTERFACE.processContent(document.getElementById(\"tab-5eef502ca56e3-1\").textContent)")
+            }
+        }
+
+        webView.loadUrl("https://eineliebe.de/infos/")
+
+        /*webView.webViewClient = object : WebViewClient() {
             override fun onReceivedError(
                 view: WebView,
                 errorCode: Int,
@@ -58,6 +77,15 @@ class AllgemeinInfosFragment : Fragment() {
             }
         }
         webView.loadUrl("https://eineliebe.de/infos/")
+
+        val webSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.javaScriptCanOpenWindowsAutomatically = true
+        webSettings.builtInZoomControls = true
+        webSettings.pluginState = WebSettings.PluginState.ON
+        webView.loadData("<iframe src=\"http://www.google.com\"></iframe>", "text/html",
+            "utf-8")*/
+
 
         return binding.root
     }
