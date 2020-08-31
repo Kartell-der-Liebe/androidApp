00e2ebe399a77b48052516c38191360e9e5d5a45
Bjoern Petersen <bjoern.petersen@missinglinkelectronics.com>
2020-08-31 20:27:12 +0200

    Expendable TextView für Anreise geschrieben

--- src/app/src/main/java/com/example/kartellderliebe/Investment.kt
+++
@@ -1,7 +0,0 @@
-package com.example.kartellderliebe
-
-class Investment(var name: String, var quantity: Int, var amount: Double) {
-    val stringAmount: String
-        get() = String.format("R$ %,.2f", amount)
-
-}
